b10b892ea161bb882b61b76fa0d8c24c7ddc1711
VijayaprakashVp <prakashvijay905@gmail.com>
2022-02-19 21:46:19 +0530

    footer added

---
+++ components/footer.js
@@ -0,0 +1,93 @@
+function footer () {
+    return `<div id="footer">
+    <div id="social_media">
+      <div id="apps">
+        <p><strong>Follow us</strong></p>
+        <img src="https://i.ibb.co/j3185gs/Screenshot-85.png" alt="" />
+        <img src="https://i.ibb.co/Z1z6PqX/Screenshot-86.png" alt="" />
+        <img src="https://i.ibb.co/VDGXnVq/Screenshot-87.png" alt="" />
+        <img src="https://i.ibb.co/197Q6DY/Screenshot-88.png" alt="" />
+        <img
+          src="https://i.ibb.co/Ct7nzg3/pinterest-social-icons-png-14.png"
+          alt=""
+        />
+      </div>
+      <div id="mobile">
+        <p><strong>TOMTOP Mobile</strong></p>
+        <div>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTM41UDKF9JzhCsASRoE1zyKp1_LwkGEM6RWQ&usqp=CAU"
+            alt=""
+            id="android"
+          />
+        </div>
+        <div>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROuEMPq9cJTk5hyH8fSBF8rgt2X7LuWxL8YQ&usqp=CAU"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+    <div id="info">
+      <div id="sub_div">
+        <h4>Company Info</h4>
+        <p>About TOMTOP</p>
+        <p>Site Map</p>
+        <p>Legal Window</p>
+        <p>Forum</p>
+        <p>Blog</p>
+      </div>
+      <div id="sub_div">
+        <h4>Payment & Shipping</h4>
+        <p>How to pay</p>
+        <p>Shipping FAQ</p>
+        <p>Ordering FAQ</p>
+      </div>
+      <div id="sub_div">
+        <h4>Policies & Service</h4>
+        <p>Terms & Conditions</p>
+        <p>Support FAQ</p>
+        <p>Privacy Policy</p>
+        <p>Reseller Ratings</p>
+        <p>Second-Hand Rules</p>
+        <p>Intellectual Property Rights</p>
+      </div>
+      <div id="sub_div">
+        <h4>Partner Program</h4>
+        <p>Get it Free</p>
+        <p>Affiliate Program</p>
+        <p>Wholesale Program</p>
+        <p>Dropship Program</p>
+        <p>Super User Program</p>
+        <p>Students Exclusive</p>
+        <p>See All</p>
+      </div>
+      <div id="subscribe">
+        <h4>Subscribe To Our Newsletter</h4>
+        <p>
+          <input
+            type="text"
+            placeholder="Enter E-mail to get $50 cash coupon"
+            class="coupon"
+          />
+        </p>
+        <br />
+        <p id="text">
+          You will receive our latest updates, news, and promotions from
+          Tomtop.
+        </p>
+        <br />
+        <p><button class="sub">Subscribe</button> &nbsp;<span> Privacy Policies</span> </p> 
+      </div>
+    </div>
+    <div>
+        <img src="https://i.ibb.co/gvvLwx5/Screenshot-90.png" alt="">
+    </div>
+    <div id="copyright">
+        <p>Copyright © 2004 - 2022 TOMTOP All Rights Reserved. (Build: 2305)</p>
+    </div>
+  </div>`
+}
+
+export default footer;
