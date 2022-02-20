88cde86ea1295cc1ec60ba1f3cdd7593e62cf21a
VijayaprakashVp <prakashvijay905@gmail.com>
2022-02-20 21:00:37 +0530

    Sign_In Created

---
+++ Scripts/slideshow.js
@@ -0,0 +1,29 @@
+function slideshow() {
+  let slideshows = document.getElementById("slideshow");
+
+  let img = document.createElement("img");
+
+  let images = [
+    "https://img.tttcdn.com/advertising/2022/2/11/xASmrG-1991557746.jpg",
+    "https://img.tttcdn.com/advertising/2022/2/9/VrgvUX-1890407756.jpg",
+    "https://img.tttcdn.com/advertising/2022/2/11/jTnJyh701652965.jpg",
+    "https://img.tttcdn.com/advertising/2022/2/11/mqpE4H560541196.jpg",
+    "https://img.tttcdn.com/advertising/2022/2/11/p7FWgn-2025521969.jpg",
+  ];
+
+  img.src = images[0];
+  var count = 1;
+  setInterval(() => {
+    if (count === images.length) {
+      count = 0;
+    }
+
+    img.src = images[count];
+    slideshows.append(img);
+    count++;
+  }, 2000);
+
+  slideshows.append(img);
+}
+
+slideshow();
