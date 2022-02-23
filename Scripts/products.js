7b33209be39e443f3f8748ac2e7c2720988ec41b
VijayaprakashVp <prakashvijay905@gmail.com>
2022-02-23 19:27:30 +0530

    Update

--- Scripts/products.js
+++ Scripts/products.js
@@ -23,22 +23,28 @@ function hello2(array) {
     link.setAttribute("target", "_blank");
 
     let div = document.createElement("div");
+    // let div2 = document.createElement("div");
+    // div2.setAttribute("id" , "product-name")
 
     let img = document.createElement("img");
     img.src = product.img_link;
 
     let name = document.createElement("p");
-    name.innerText = product.product_name;
+    name.innerText = "Title :   " + product.product_name;
 
     let cur_price = document.createElement("p");
-    cur_price.innerText = product.current_price;
+    cur_price.innerText = "Discount price : " + product.current_price;
+    cur_price.setAttribute("id", "dis_price");
 
     let reg_price = document.createElement("p");
     reg_price.innerText = product.regular_price;
+    reg_price.setAttribute("id", "regular_price");
 
     div.append(img, name, cur_price, reg_price);
-    // console.log((img, name, cur_price, reg_price));
+    // div2.append();
     link.append(div);
+    // console.log((img, name, cur_price, reg_price));
+    // Social_saving.append(link);
     all_product.append(link);
   });
 }
