7c292ac37d6355e2dfaea1dd096b58144ece2ddc
VijayaprakashVp <prakashvijay905@gmail.com>
2022-02-23 19:47:26 +0530

    Top_brands.js

---
+++ components/top_brands.js
@@ -0,0 +1,74 @@
+function topbrand () {
+    return `<a href="#"
+    ><img
+      src="https://img.tttcdn.com/file/attach/2018/0/8/CNF814-Zeblaze.jpg"
+      alt=""
+  /></a>
+  <a href="#"
+    ><img
+      src="https://img.tttcdn.com/product/brand/2020/11/23/5Eh0lK46728356.jpg"
+      alt=""
+  /></a>
+  <a href="#"
+    ><img
+      src="https://img.tttcdn.com/product/brand/2019/2/11/Gm0toG548617673.jpg"
+      alt=""
+  /></a>
+  <a href="#"
+    ><img
+      src="https://img.tttcdn.com/product/brand/2019/9/19/v7G4af46728356.jpg"
+      alt=""
+  /></a>
+  <a href="#"
+    ><img
+      src="https://img.tttcdn.com/product/brand/2020/6/3/X0MPuW1068267905.jpg"
+      alt=""
+  /></a>
+  <a href="#"
+    ><img
+      src="https://img.tttcdn.com/product/brand/2019/2/11/d703Jf-1834503252.jpg"
+      alt=""
+  /></a>
+  <a href="#"
+    ><img
+      src="https://img.tttcdn.com/product/brand/2020/5/30/SVhmcT-1529418360.png"
+      alt=""
+  /></a>
+  <a href="#"
+    ><img
+      src="https://img.tttcdn.com/product/brand/2019/2/11/qpLeD31431755671.jpg"
+      alt=""
+  /></a>
+  <a href="#"
+    ><img
+      src="https://img.tttcdn.com/product/brand/2019/2/11/Epa2dc-932118670.jpg"
+      alt=""
+  /></a>
+  <a href="#"
+    ><img
+      src="https://img.tttcdn.com/product/brand/2019/2/11/zdYTSR346553119.jpg"
+      alt=""
+  /></a>
+  <a href="#"
+    ><img
+      src="https://img.tttcdn.com/product/brand/2020/5/30/prP3Ss-290551226.png"
+      alt=""
+  /></a>
+  <a href="#"
+    ><img
+      src="https://img.tttcdn.com/product/brand/2019/2/11/x1fyZm-717839356.jpg"
+      alt=""
+  /></a>
+  <a href="#"
+    ><img
+      src="https://img.tttcdn.com/product/brand/2019/2/11/FgnmpU-1635887911.jpg"
+      alt=""
+  /></a>
+  <a href="#"
+    ><img
+      src="https://img.tttcdn.com/product/brand/2019/2/11/Woa2e51079989138.jpg"
+      alt=""
+  /></a>`
+}
+
+export default topbrand;
